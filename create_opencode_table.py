826af0b9e5cb7e9899b272d9a3b37b4c09dbe983
Jimmy Wu <jimmy.wu@daedalus.com.tw>
2020-06-21 14:15:33 +0800

    產生解答表程式整理

--- create_opencode_table.py
+++ create_opencode_table.py
@@ -83,6 +83,44 @@ def TSZF2(balls, num1, num2):
     result2 = balls[0] == num2 and balls[1] == num1 
     return 1 if ( result1 or result2 ) else -1
 
+def createHeader():
+    headers = [ "opencode" ]
+    for i in ALL_BALLS:
+        for j in ALL_BALLS:
+            headers.append(f"DWD{i}_{j}")
+    for (i, j, k) in list(itertools.permutations(ALL_BALLS, 3)):
+        headers.append(f"TZF3_{i}-{j}-{k}")
+    for (i, j) in list(itertools.permutations(ALL_BALLS, 2)):
+        headers.append(f"TZF2_{i}-{j}")
+    for i in ALL_BALLS:
+        headers.append(f"TZF1_{i}")
+    for i in ALL_BALLS:
+        headers.append(f"B{i}")   
+    for i in ALL_BALLS:
+        headers.append(f"S{i}") 
+    for i in ALL_BALLS:
+        headers.append(f"O{i}") 
+    for i in ALL_BALLS:
+        headers.append(f"E{i}") 
+    for i in ALL_BALLS:
+        headers.append(f"P{i}") 
+    for i in ALL_BALLS:
+        headers.append(f"C{i}") 
+    for i in range(1,6):
+        headers.append(f"D{i}") 
+    for i in range(1,6):
+        headers.append(f"T{i}")
+    for i in range(3,20):
+        headers.append(f"SUM{i}") 
+    headers.append(f"SUMB")
+    headers.append(f"SUMS")
+    headers.append(f"SUMO")
+    headers.append(f"SUME")
+    for (i, j) in list(itertools.combinations(ALL_BALLS, 2)):
+        headers.append(f"TSZF2_{i}-{j}")
+    return headers
+
+ALL_BALLS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def main():
     parser = ArgumentParser()
     parser.add_argument("--header", help="optional argument", dest="header", default=False)
@@ -90,189 +128,158 @@ def main():
     hasHeader = bool(args.header)
     print("header arg:", hasHeader)
 
-    all_balls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    ballsList = [(1, 2, 3, 4, 5, 6, 7, 8, 10, 9)]
-    #ballsList = list(itertools.permutations(all_balls, 10))
+    #ballsList = [(1, 2, 3, 4, 5, 6, 7, 8, 10, 9)]
+    ballsList = list(itertools.permutations(ALL_BALLS, 10))
 
+    row_max_length = 20
+    row_count = 0
     with open('opencode_table.csv', 'w+', encoding='UTF-8') as f:
-        #rows = []
-        
-        columns = [ "opencode" ]
-        for i in all_balls:
-            for j in all_balls:
-                columns.append(f"DWD{i}_{j}")
-        for (i, j, k) in list(itertools.permutations(all_balls, 3)):
-            columns.append(f"TZF3_{i}-{j}-{k}")
-        for (i, j) in list(itertools.permutations(all_balls, 2)):
-            columns.append(f"TZF2_{i}-{j}")
-        for i in all_balls:
-            columns.append(f"TZF1_{i}")
-        for i in all_balls:
-            columns.append(f"B{i}")   
-        for i in all_balls:
-            columns.append(f"S{i}") 
-        for i in all_balls:
-            columns.append(f"O{i}") 
-        for i in all_balls:
-            columns.append(f"E{i}") 
-        for i in all_balls:
-            columns.append(f"P{i}") 
-        for i in all_balls:
-            columns.append(f"C{i}") 
-        for i in range(1,6):
-            columns.append(f"D{i}") 
-        for i in range(1,6):
-            columns.append(f"T{i}")
-        for i in range(3,20):
-            columns.append(f"SUM{i}") 
-        columns.append(f"SUMB")
-        columns.append(f"SUMS")
-        columns.append(f"SUMO")
-        columns.append(f"SUME")
-        for (i, j) in list(itertools.combinations(all_balls, 2)):
-            columns.append(f"TSZF2_{i}-{j}")
-
-        str3 = ','.join(str(e) for e in columns)
-        f.write(str3 + "\n")
-        print(f"columns length:{len(columns)}")
+        headers = createHeader()
+        if hasHeader:
+            strHeaderWithComma = ','.join(str(e) for e in headers)
+            f.write(strHeaderWithComma + "\n")
+            print(f"header length:{len(headers)}")
 
         for balls in ballsList: # ALL
             print(balls)
-            if balls[0] >= 3:
+
+            if row_count == row_max_length:
                 break
 
-            cols = []
-            for column in columns:
-                if re.match("^DWD([0-9]{1,2})_([0-9]{1,2})$", column):
-                    pos = int(column.split("_")[0].replace("DWD", ""))
-                    num = int(column.split("_")[1])
+            columns = []
+            for header in headers:
+                if re.match("^DWD([0-9]{1,2})_([0-9]{1,2})$", header):
+                    pos = int(header.split("_")[0].replace("DWD", ""))
+                    num = int(header.split("_")[1])
                     ret = DWD(balls, pos, num) 
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"DWD{pos}_{num} = {ret}")
-                elif re.match("^TZF3_([0-9]{1,2})-([0-9]{1,2})-([0-9]{1,2})$", column):
-                    nums = column.replace("TZF3_", "")
+                elif re.match("^TZF3_([0-9]{1,2})-([0-9]{1,2})-([0-9]{1,2})$", header):
+                    nums = header.replace("TZF3_", "")
                     num1 = int(nums.split("-")[0])
                     num2 = int(nums.split("-")[1])
                     num3 = int(nums.split("-")[2])
                     ret = TFZ3(balls, num1, num2, num3)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"TZF3_{num1}-{num2}-{num3} = {ret}")
-                elif re.match("^TZF2_([0-9]{1,2})-([0-9]{1,2})$", column):
-                    nums = column.replace("TZF2_", "")
+                elif re.match("^TZF2_([0-9]{1,2})-([0-9]{1,2})$", header):
+                    nums = header.replace("TZF2_", "")
                     num1 = int(nums.split("-")[0])
                     num2 = int(nums.split("-")[1])
                     ret = TFZ2(balls, num1, num2)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"TZF2_{num1}-{num2} = {ret}")
-                elif re.match("^TZF1_([0-9]{1,2})$", column):
-                    num = int(column.replace("TZF1_", ""))
+                elif re.match("^TZF1_([0-9]{1,2})$", header):
+                    num = int(header.replace("TZF1_", ""))
                     ret = TFZ1(balls, num)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"TZF1_{num} = {ret}")
-                elif re.match("^B([0-9]{1,2})$", column):
-                    pos = int(column.replace("B", ""))
+                elif re.match("^B([0-9]{1,2})$", header):
+                    pos = int(header.replace("B", ""))
                     ret = B(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"B{pos} = {ret}")
-                elif re.match("^S([0-9]{1,2})$", column):
-                    pos = int(column.replace("S", ""))
+                elif re.match("^S([0-9]{1,2})$", header):
+                    pos = int(header.replace("S", ""))
                     ret = S(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"S{pos} = {ret}")
-                elif re.match("^O([0-9]{1,2})$", column):
-                    pos = int(column.replace("O", ""))
+                elif re.match("^O([0-9]{1,2})$", header):
+                    pos = int(header.replace("O", ""))
                     ret = O(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"O{pos} = {ret}")
-                elif re.match("^E([0-9]{1,2})$", column):
-                    pos = int(column.replace("E", ""))
+                elif re.match("^E([0-9]{1,2})$", header):
+                    pos = int(header.replace("E", ""))
                     ret = E(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"E{pos} = {ret}")
-                elif re.match("^P([0-9]{1,2})$", column):
-                    pos = int(column.replace("P", ""))
+                elif re.match("^P([0-9]{1,2})$", header):
+                    pos = int(header.replace("P", ""))
                     ret = P(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"P{pos} = {ret}")
-                elif re.match("^C([0-9]{1,2})$", column):
-                    pos = int(column.replace("C", ""))
+                elif re.match("^C([0-9]{1,2})$", header):
+                    pos = int(header.replace("C", ""))
                     ret = C(balls, pos)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"C{pos} = {ret}")
-                elif column == "D1":
+                elif header == "D1":
                     ret = D1(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"D1 = {ret}")
-                elif column == "D2":
+                elif header == "D2":
                     ret = D2(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"D2 = {ret}")
-                elif column == "D3":
+                elif header == "D3":
                     ret = D3(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"D3 = {ret}")
-                elif column == "D4":
+                elif header == "D4":
                     ret = D4(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"D4 = {ret}")
-                elif column == "D5":
+                elif header == "D5":
                     ret = D5(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"D5 = {ret}")
-                elif column == "T1":
+                elif header == "T1":
                     ret = T1(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"T1 = {ret}")
-                elif column == "T2":
+                elif header == "T2":
                     ret = T2(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"T2 = {ret}")
-                elif column == "T3":
+                elif header == "T3":
                     ret = T3(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"T3 = {ret}")
-                elif column == "T4":
+                elif header == "T4":
                     ret = T4(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"T4 = {ret}")
-                elif column == "T5":
+                elif header == "T5":
                     ret = T5(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"T5 = {ret}")
-                elif re.match("^SUM([0-9]{1,2})$", column):
-                    num = int(column.replace("SUM", ""))
+                elif re.match("^SUM([0-9]{1,2})$", header):
+                    num = int(header.replace("SUM", ""))
                     ret = SUM(balls, num)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"SUM{num} = {ret}")
-                elif column == "SUMB":
+                elif header == "SUMB":
                     ret = SUMB(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"SUMB = {ret}")
-                elif column == "SUMS":
+                elif header == "SUMS":
                     ret = SUMS(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"SUMS = {ret}")
-                elif column == "SUMO":
+                elif header == "SUMO":
                     ret = SUMO(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"SUMO = {ret}")
-                elif column == "SUME":
+                elif header == "SUME":
                     ret = SUME(balls)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"SUME = {ret}")
-                elif re.match("^TSZF2_([0-9]{1,2})-([0-9]{1,2})$", column):
-                    nums = column.replace("TSZF2_", "")
+                elif re.match("^TSZF2_([0-9]{1,2})-([0-9]{1,2})$", header):
+                    nums = header.replace("TSZF2_", "")
                     num1 = int(nums.split("-")[0])
                     num2 = int(nums.split("-")[1])
                     ret = TSZF2(balls, num1, num2)
-                    cols.append(ret)
+                    columns.append(ret)
                     #print(f"TSZF2_{num1}-{num2} = {ret}")
-            
-            print(f"cols length:{len(cols)}")
-            #rows.append(cols)
-            str1 = '-'.join(str(e) for e in balls)
-            str2 = ','.join(str(e) for e in cols)
-            f.write(str1 + "," + str2 + "\n")
+
+            row_count +=1
+
+            #print(f"columns length:{len(columns)}")
+            #rows.append(columns)
+            strBalls = '-'.join(str(e) for e in balls)
+            strColumnsWithComma = ','.join(str(e) for e in columns)
+            f.write(strBalls + "," + strColumnsWithComma + "\n")
 
     #print(rows)
 if __name__ == '__main__':
