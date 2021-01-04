6062d4a5086dda2d9516acb3c57deefe12c18778
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-01-04 17:52:26 +0800

    修正擷取字串後有亂碼

--- src/mystring.c
+++ src/mystring.c
@@ -54,8 +54,10 @@ char* substring(const char s_src[], int i_start, int i_end)
     char * p_stmp;
     int substrlength = i_end - i_start ;
     printf ("==========>Malloc size:%i\n", substrlength);
-    p_stmp = (char *) malloc(sizeof(char) * (substrlength + 1));
-    memset(p_stmp, '\0', sizeof(char) * (substrlength + 1));
-    strncpy(p_stmp,s_src + i_start, substrlength);
+    log_debug("substring before : %s", s_src);
+    p_stmp = (char *) malloc(sizeof(char) * (substrlength + 2)); //+1會有後面亂碼的問題
+    memset(p_stmp, '\0', sizeof(char) * (substrlength + 2));
+    strncpy(p_stmp, s_src + i_start, substrlength);
+    log_debug("substring after : %s", s_src);
     return p_stmp;
 }
