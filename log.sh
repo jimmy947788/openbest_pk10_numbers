4a4a5654bf52feed364df2d042b1cf2cd2d9259e
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-01-06 11:53:54 +0800

    位完成封存log腳本

---
+++ log.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+WORKFOLDER="/home/matrix/openbest_pk10_numbers/log"
+TODAY=$(date +"%Y-%m-%d")
+echo $WORKFOLDER/calc_opencode_amount-$TODAY.log 
+#cp $WORKFOLDER/calc_opencode_amount.log $WORKFOLDER/calc_opencode_amount_.log
