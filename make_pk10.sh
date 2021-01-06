f99f652bb88b236079a1c0748cb379284312800b
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-01-06 17:26:29 +0800

    編譯前先clean

--- make_pk10.sh
+++ make_pk10.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VER=$1
+make clean
 if [ "$VER" == debug ];
 then
 	make lotterykind=pk10 ver=debug
--- make_ssc.sh
+++ make_ssc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VER=$1
+make clean
 if [ "$VER" == debug ];
 then
 	make lotterykind=ssc ver=debug
