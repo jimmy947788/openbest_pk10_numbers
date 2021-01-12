2e41fad65af72b04e4fce1f2e8f7c0528af31199
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-01-12 15:59:34 +0800

    找不到GPU重啟

--- run.bash
+++ run.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-currentPath="$(pwd)"
+#currentPath="$(pwd)"
 #printf "current path : %s\n" $currentPath
-#WORKFOLDER="/home/matrix/openbest_pk10_numbers"
-WORKFOLDER="$currentPath"
+#WORKFOLDER="$currentPath"
+
+WORKFOLDER="/home/matrix/openbest_pk10_numbers"
 
 SERVICE="optimize_opencode"
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
@@ -17,6 +18,14 @@ else
     "$WORKFOLDER"/bin/$SERVICE \
         --worker-folder  "$WORKFOLDER" \
         --kernel-program bin/kernel_program.cl
+fi
+
+tmp0=$(nvidia-smi -i 0 | grep "Default" | awk '{print $3}') #溫度
+tmp1=$(nvidia-smi -i 1 | grep "Default" | awk '{print $3}') #溫度
+if [ -z "$tmp0" ] && [ -z "$tmp1" ]
+then
+    #echo "tmp0=$tmp0, tmp1=$tmp1"
+    reboot
 fi
 
 #sudo gdb bin/optimize_opencode
