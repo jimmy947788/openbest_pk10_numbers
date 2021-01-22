649a328b2b24e769e359d4cdd5c16ad212fd85c0
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-01-22 15:07:53 +0800

    更新初始化系統腳本

--- inti-system.sh
+++ inti-system.sh
@@ -8,3 +8,9 @@ apt-get install net-tools hwinfo vim clinfo ocl-icd-libopencl1 opencl-headers oc
 sudo -H pip3 install Flask
 sudo -H pip3 install pandas
 sudo -H pip3 install pyopencl
+
+#SWAP加到10G
+# https://shazi.info/ubuntu-16-04-%E5%BB%BA%E7%AB%8B-swap-file-%E8%AA%BF%E6%A0%A1-swap/
+
+# 更新linux kernel 5
+sudo dpkg -i  kernel-5.9.16/*.deb
