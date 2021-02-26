9452f666000a6af34eda58bebbfd1dca46c95858
jimmy947788 <jimmy.wu@daedalus.com.tw>
2021-02-27 02:01:38 +0800

    新增uint64，int64

--- header/mytype.h
+++ header/mytype.h
@@ -2,10 +2,12 @@
 #define MYTYPE_H_
 
 typedef unsigned char boolean; /* Boolean value type. */
+typedef unsigned long long uint64;
 typedef unsigned long int uint32; /* Unsigned 32 bit value */
 typedef unsigned short uint16; /* Unsigned 16 bit value */
 typedef unsigned char uint8; /* Unsigned 8 bit value */
 
+typedef signed long long int64;
 typedef signed long int int32; /* Signed 32 bit value */
 typedef signed short int16; /* Signed 16 bit value */
 typedef signed char int8; /* Signed 8 bit value */
